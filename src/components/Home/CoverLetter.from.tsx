56f42095298deb827d8080a12373f1aef85a14c0
Armin Basirian <armin@socialteq.co>
2021-10-07 10:04:44 +0330

    Cover letter form

---
+++ src/components/Home/CoverLetter.from.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as yup from 'yup';
+
+import { Box, Input, Submit } from 'components';
+
+const validationSchema = yup.object().shape({
+  company: yup.string().required('No Company Name provided'),
+  job_title: yup.string().required('No Job Title provided'),
+});
+
+export default ({ onSubmit, submitting }: any): JSX.Element => {
+  return (
+    <Formik
+      initialValues={{
+        company: '',
+        job_title: '',
+      }}
+      validateOnMount
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {(formik) => {
+        return (
+          <Box width="100%" as={Form}>
+            <Box width="100%" marginBottom="45px">
+              <Input
+                formik={formik}
+                label="Company Name"
+                name="company"
+                type="text"
+                placeholder="Enter Company Name"
+              />
+            </Box>
+            <Box width="100%" marginBottom="45px">
+              <Input
+                formik={formik}
+                label="Job Title"
+                name="job_title"
+                type="text"
+                placeholder="Enter Job Title"
+              />
+            </Box>
+            <Submit
+              size="medium"
+              type="secondary"
+              shape="fill"
+              width="100%"
+              formik={formik}
+              loading={submitting}
+              label="Send"
+            />
+          </Box>
+        );
+      }}
+    </Formik>
+  );
+};
